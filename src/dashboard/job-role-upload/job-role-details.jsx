e751497d4cc9f64361d2ca82172c8b108f5f0901
Precious Udegbue <Precious.Udegbue@sterling.ng>
2024-06-24 11:34:45 +0100

    worked on job role details

--- src/dashboard/job-role-upload/job-role-details.jsx
+++ src/dashboard/job-role-upload/job-role-details.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import back from '../../assets/back-icon.svg'
 import './job-role.css';
+import jobRoleDetail from '../../assets/job-role-detail.svg';
+import jobRoleInfo from '../../assets/job-role-info.svg';
 import JobRoleForm from './job-role-form';
 
 const JobRoleDetails = ({ selectedUser, setToggleViews }) => {
   const [showJobForm, setShowJobForm] = React.useState(false)
+  console.log({selectedUser})
 
   return (
     <>
@@ -18,7 +21,7 @@ const JobRoleDetails = ({ selectedUser, setToggleViews }) => {
       <div className='job-details-header h-64 flex items-center mt-3'>
         <div className='flex items-center justify-between w-full px-20'>
           <div className='flex items-center gap-2'>
-            <div className='bg-white rounded-full p-8 font-semibold text-center custom-green text-5xl flex m-auto'>JD</div>
+            <div className='bg-white rounded-full p-8 font-semibold uppercase text-center custom-green text-5xl flex m-auto'>{selectedUser?.employeeName.slice(0,2)}</div>
             <div className=''>
               <div className='custom-green font-medium text-xl'>Jonathan Donald</div>
               <div className='custom-green text-base'>Jonathan.donald@gmail.com</div>
@@ -26,6 +29,45 @@ const JobRoleDetails = ({ selectedUser, setToggleViews }) => {
           </div>
           <button onClick={() => setShowJobForm(true)} className='w-80 custom-bg-green p-2 rounded-lg text-white text-sm font-bold'>Add details</button>
         </div>
+      </div>
+
+      <div className='flex flex-col mt-5 gap-5 px-10'>
+
+       <div className='flex justify-between'>
+        <div className=' flex flex-col gap-1'>
+          <div className='font-semibold text-base light-grey text-center'>Department</div>
+          <div className='text-base flex items-center gap-2'><img src={jobRoleDetail} alt='job-role-icon' /> <span className='text-black font-bold text-lg'>Software developer</span></div>
+        </div>
+
+        <div className=' flex flex-col gap-1'>
+          <div className='font-semibold text-base light-grey text-center'>Department</div>
+          <div className='text-base flex items-center gap-2'><img src={jobRoleDetail} alt='job-role-icon' /> <span className='text-black font-bold text-lg'>Software developer</span></div>
+        </div>
+
+        <div className=' flex flex-col gap-1'>
+          <div className='font-semibold text-base light-grey text-center'>Department</div>
+          <div className='text-base flex items-center gap-2'><img src={jobRoleDetail} alt='job-role-icon' /> <span className='text-black font-bold text-lg'>Software developer</span></div>
+        </div>
+
+       </div>
+
+       <div className='flex justify-between'>
+        <div className=' flex flex-col gap-1'>
+          <div className='font-semibold text-base light-grey text-center'>Department</div>
+          <div className='text-base flex items-center gap-2'><img src={jobRoleInfo} alt='job-role-icon' /> <span className='text-black font-bold text-lg'>Software developer</span></div>
+        </div>
+
+        <div className=' flex flex-col gap-1'>
+          <div className='font-semibold text-base light-grey text-center'>Department</div>
+          <div className='text-base flex items-center gap-2'><img src={jobRoleInfo} alt='job-role-icon' /> <span className='text-black font-bold text-lg'>Software developer</span></div>
+        </div>
+
+        <div className=' flex flex-col gap-1'>
+          <div className='font-semibold text-base light-grey text-center'>Department</div>
+          <div className='text-base flex items-center gap-2'><img src={jobRoleInfo} alt='job-role-icon' /> <span className='text-black font-bold text-lg'>Software developer</span></div>
+        </div>
+
+       </div>
       </div>
     </div>}
     </>
